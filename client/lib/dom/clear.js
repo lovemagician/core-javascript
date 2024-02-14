3af532a5c316cb4f558952b958864329c325f7a6
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-02-14 10:19:55 +0900

    learn js

---
+++ client/lib/dom/clear.js
@@ -0,0 +1,11 @@
+import { getNode } from './getNode.js';
+
+export function clearContents(node) {
+  if (typeof node === 'string') node = getNode(node);
+
+  if (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA') {
+    node.value = '';
+    return;
+  }
+  node.textContent = '';
+}
--- client/main.js
+++ client/main.js
@@ -1,3 +1,60 @@
-const a = 10;
+/* global getNode, insertLast, clearContents, getNodes */
 
-console.log("안녕 자바스크립트!");
+import {
+  getNode,
+  getNodes,
+  insertLast,
+  clearContents,
+  refError,
+  typeError,
+} from './lib/index.js';
+
+function phase1() {
+  const first = getNode('#firstNumber');
+  const second = getNode('#secondNumber');
+  const result = getNode('.result');
+  const clear = getNode('#clear');
+
+  function handleInput() {
+    const firstValue = Number(first.value);
+    const secondValue = Number(second.value);
+    const total = firstValue + secondValue;
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+const calculator = getNode('.calculator');
+const result = getNode('.result');
+const clear = getNode('#clear');
+const numberInputs = Array.from(getNodes('input:not(#clear)'));
+
+function handleInput() {
+  const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+function handleClear(e) {
+  e.preventDefault();
+
+  numberInputs.forEach(clearContents);
+
+  result.textContent = '-';
+}
+
+calculator.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
